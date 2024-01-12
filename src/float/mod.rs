749ca8b9ecdba8d09abe09ad23bd57c25e9a95ba
Trevor Spiteri <tspiteri@ieee.org>
2024-01-12 11:08:34 +0100

    update copyright year

--- src/float/mod.rs
+++ src/float/mod.rs
@@ -1,4 +1,4 @@
-// Copyright © 2016–2023 Trevor Spiteri
+// Copyright © 2016–2024 Trevor Spiteri
 
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the Free
