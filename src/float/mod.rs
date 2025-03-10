7e31940986328be85132a84ce7c6574b7b62db7d
Kartik Soneji <kartiksoneji@rocketmail.com>
2025-03-10 20:39:40 +0530

    Implement borsh for `Float`

--- src/float/mod.rs
+++ src/float/mod.rs
@@ -24,6 +24,8 @@ This module provides support for floating-point numbers of type
 pub(crate) mod arith;
 pub(crate) mod big;
 mod borrow;
+#[cfg(feature = "borsh")]
+mod borsh;
 mod casts;
 mod cmp;
 #[cfg(feature = "num-traits")]
